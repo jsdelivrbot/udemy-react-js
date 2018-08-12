a9e6777ba1e7f14c82a37c03e0d5b3ecded6d088
Amir Nursultan <amir.nursultan@yandex.ru>
2018-08-12 20:02:08 +0600

    first commit

---
+++ book-list-react/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry potter', pages: 101 },
+    { title: 'Garry kotter', pages: 69 },
+    { title: 'Mary Spider woman', pages: 25 },
+    { title: 'Wonder Batwoman', pages: 34 }
+  ]
+}
